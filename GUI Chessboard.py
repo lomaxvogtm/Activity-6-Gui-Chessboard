955ef13d3f0f4680c2388704cf7d657e417de092
lomaxvogtm <mcl9@alfred.edu>
2015-03-08 21:49:05 -0400

    Exam 2: Honestly, just take what I have.  I'll come in during office hours and talk to you about what I did wrong after break.  Have a great break!

---
+++ GUI Chessboard.py
@@ -0,0 +1,22 @@
+__author__ = 'Madeleine'
+
+
+import tkinter as tk
+def board_squares():
+    for row in range(8):
+        for column in range(8):
+            board.create_rectangle((100*row+50), (100*column+50), (100*row+150), (100*column+150), fill='white',outline="black")
+
+root = tk.Tk()
+board = tk.Canvas(root, width = 900, height = 900, bg='gray')
+board.grid(row=0, column=0)
+board_squares()
+root.mainloop()
+
+
+
+
+
+
+
+
