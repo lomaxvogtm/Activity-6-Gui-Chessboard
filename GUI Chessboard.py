d97bbccb176400f7b01842cb9ea6daa1137ed1f8
lomaxvogtm <mcl9@alfred.edu>
2015-03-18 15:55:46 -0400

    Chessboard

--- GUI Chessboard.py
+++ GUI Chessboard.py
@@ -5,7 +5,11 @@ import tkinter as tk
 def board_squares():
     for row in range(8):
         for column in range(8):
-            board.create_rectangle((100*row+50), (100*column+50), (100*row+150), (100*column+150), fill='white',outline="black")
+            if (row + column) % 2 == 0:
+                board.create_rectangle((100*row+50), (100*column+50), (100*row+150), (100*column+150), fill='white',outline="black")
+            else:
+                board.create_rectangle((100*row+50), (100*column+50), (100*row+150), (100*column+150), fill='black',outline="black")
+
 
 root = tk.Tk()
 board = tk.Canvas(root, width = 900, height = 900, bg='gray')
